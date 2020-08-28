96cd372e1d8cd632dc351e11a8a5db2042f2f8cd
chuahbc <booncheng.cbc@gmail.com>
2020-08-28 13:48:53 +0800

    Table Printer completed

--- Chapter06/TablePrinter.py
+++ Chapter06/TablePrinter.py
@@ -2,9 +2,10 @@
 # table with each column right-justified. Assume that all the inner lists will contain the same number of strings.
 # For example, the value could look like this:
 
-tableData = [['apples', 'oranges', 'cherries', 'banana'],
-             ['Alice', 'Bob', 'Carol', 'David'],
-             ['dogs', 'cats', 'moose', 'goose']]
+tableData = [['apples', 'oranges', 'cherries', 'banana', 'watermelon'],
+             ['Alice', 'Bob', 'Carol', 'David', 'John'],
+             ['dogs', 'cats', 'moose', 'goose', 'chicken'],
+             ['socks', 'shoes', 'shirt', 'pants','necktie']]
 
 # Your printTable() function would print the following:
 
@@ -19,3 +20,12 @@ tableData = [['apples', 'oranges', 'cherries', 'banana'],
 # number of 0 values as the number of inner lists in tableData. That way, colWidths[0] can store the width of the longest 
 # string in tableData[0], colWidths[1] can store the width of the longest string in tableData[1], and so on. You can 
 # then find the largest value in the colWidths list to find out what integer width to pass to the rjust() string method.
+
+reArranged = []
+
+for i in range(len(tableData[0])):
+    for n in tableData:
+        reArranged.append(n[i])
+    print(' '.join(reArranged))
+    reArranged = []
+
